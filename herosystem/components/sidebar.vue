292c819f65f956946416da7c5ef9fbad0a6700d7
TheDarkN1ght001 <1132962475@qq.com>
2019-06-18 16:56:01 +0800

    修复：给链接设置点击后的类名
    active-class
    tag

--- herosystem/components/sidebar.vue
+++ herosystem/components/sidebar.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="col-sm-3 col-md-2 sidebar">
       <ul class="nav nav-sidebar">
-        <li class="active">
-          <router-link to="/herolist">英雄管理列表</router-link>
-        </li>
-        <li>
-          <router-link to="/jineng">技能管理列表</router-link>
-        </li>
-        <li>
-          <router-link to="/wuqi">武器管理列表</router-link>
-        </li>
+
+          <router-link to="/herolist" active-class='active' tag='li'>
+            <a>英雄管理列表</a>
+          </router-link>
+
+
+          <router-link to="/jineng" active-class='active' tag='li'>
+            <a>技能管理列表</a>
+          </router-link>
+
+
+          <router-link to="/wuqi" active-class='active' tag='li'>
+            <a>武器管理列表</a>
+          </router-link>
+
       </ul>
     </div>
 </template>
