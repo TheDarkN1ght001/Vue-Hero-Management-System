4db87fdb3d1690cfb7de3ee57b55c590ff8603fe
TheDarkN1ght001 <1132962475@qq.com>
2019-06-18 15:40:27 +0800

    bootstrap事项
    下载引入了bootstrap 并在main.js中引入了bootstrap的css文件和index.css文件 字体文件的bug 通过找到webpack.config.js 文件中的 module -> rules -> 最后一项 -> 复制，粘贴到最后一项后面，修改内容为： ttf, woff2, woff, eot。修改完配置文件之后，需要重启服务器

--- herosystem/src/main.js
+++ herosystem/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//引入bootstrap样式文件
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+//引入index.css样式文件 （给模板用的）
+import '../src/assets/css/index.css'
 new Vue({
   el: '#app',
   render: h => h(App)
